af0f18d35b58c922e4c18e11e791b6297616cc3c
maurosaravia <31512620+maurosaravia@users.noreply.github.com>
2022-06-10 13:01:54 -0300

    categories service try catch

--- src/categories/services/categories.service.ts
+++ src/categories/services/categories.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from '@categories/entities/category.entity';
@@ -10,17 +14,29 @@ export class CategoriesService {
   ) {}
 
   async exists(id: number): Promise<boolean> {
-    return (await this.categoriesRepo.count({ id: id })) > 0;
+    try {
+      return (await this.categoriesRepo.count({ id: id })) > 0;
+    } catch (exception) {
+      throw new InternalServerErrorException();
+    }
   }
 
   findAll(): Promise<Category[]> {
-    return this.categoriesRepo.find();
+    try {
+      return this.categoriesRepo.find();
+    } catch (exception) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async findOne(id: number): Promise<Category> {
     if (!(await this.exists(id))) {
       throw new NotFoundException();
     }
-    return this.categoriesRepo.findOne(id);
+    try {
+      return this.categoriesRepo.findOne(id);
+    } catch (exception) {
+      throw new InternalServerErrorException();
+    }
   }
 }
