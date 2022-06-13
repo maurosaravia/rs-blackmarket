f779b3de4f6988f9e4d7fdd3c218e29e06bb5540
maurosaravia <31512620+maurosaravia@users.noreply.github.com>
2022-06-13 10:24:51 -0300

    category entity modified
    - Added deletedAt column
    - Updated migration to include new column
    - Added validations to DTO

--- src/categories/dtos/category.dto.ts
+++ src/categories/dtos/category.dto.ts
@@ -1,9 +1,18 @@
-import { IsNumber, MaxLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CategoryDTO {
+  @IsString()
+  @IsNotEmpty()
   @MaxLength(50)
   name: string;
 
+  @IsOptional()
   @IsNumber()
   parentCategoryId?: number;
 }
