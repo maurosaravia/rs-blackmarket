7275665e505714ad007deeddbc7c566baba5cb09
maurosaravia <31512620+maurosaravia@users.noreply.github.com>
2022-07-11 16:36:39 -0300

    Unique email validation

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '@users/entities/user.entity';
 import { UserDTO } from '@users/dtos/user.dto';
+import { BadRequestException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -10,6 +11,12 @@ export class UsersRepository extends Repository<User> {
 
   createUser(createUserDto: UserDTO): Promise<User> {
     const user = this.create(createUserDto);
-    return this.save(user);
+    return this.save(user).catch((e) => {
+      if (e.routine == '_bt_check_unique')
+        throw new BadRequestException(
+          'Email is already in use, choose a new one',
+        );
+      else throw e;
+    });
   }
 }
